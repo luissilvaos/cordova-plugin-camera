b7d62f1b13000b733e4a139dd7875216cf294787
Luís Silva <luis.silva@outsystems.com>
2017-08-23 17:22:16 +0100

    Refactoring

---
+++ tests/android/CDVCameraTest/app/src/test/java/org/apache/cordova/camera/CameraUnitTest.java
@@ -0,0 +1,29 @@
+package org.apache.cordova.camera;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static android.icu.text.Normalizer.NO;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class CameraUnitTest {
+
+    private CameraLauncher plugin;
+
+    @Before
+    public void setUp() {
+        this.plugin = new CameraLauncher();
+    }
+
+
+    @Test
+    public void testPictureOptionsCreate() throws Exception {
+
+    }
+
+}
